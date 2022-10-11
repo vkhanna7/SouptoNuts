e6a771937c19a7258028135e9dd3abcf39616810
vkhanna7 <vkhanna7@submit2.chtc.wisc.edu>
2022-10-11 11:50:56 -0500

    last successful run files with output file

--- example.sh
+++ example.sh
@@ -2,33 +2,32 @@
 cp /staging/vkhanna7/diploshic.tar.gz ./
 cp -r /staging/vkhanna7/exampleApplication ./
 
-ENVNAME=diploshic
-ENVDIR=$ENVNAME
-mkdir $ENVDIR
-export PATH
-tar -xzf $ENVNAME.tar.gz -C $ENVDIR
-. $ENVDIR/bin/activate
-
-tar -xzf exampleApplication.tar.gz 
+tar -xzf packages.tar.gz
+tar -xzf python39.tar.gz
+export PATH=$PWD/python/bin:$PATH
+export PYTHONPATH=$PWD/packages
+export HOME=$PWD
+export PATH=$PWD/packages/bin/:$PATH
+#rm diploshic.tar.gz
 
 # modify this line to run your desired Python script and any other work you need to do
 rm diploshic.tar.gz
-for f in exampleApplication/*.msOut.gz; do diploSHIC fvecSim diploid $f $f.diploid.fvec --totalPhysLen 55000 --maskFileName exampleApplication/Anopheles-gambiae-PEST_CHROMOSOMES_AgamP3.accessible.fa.gz --chrArmsForMasking 3R & done
+for f in exampleApplication/*.msOut.gz; do python3 ./packages/bin/diploSHIC fvecSim diploid $f $f.diploid.fvec --totalPhysLen 55000 --maskFileName exampleApplication/Anopheles-gambiae-PEST_CHROMOSOMES_AgamP3.accessible.fa.gz --chrArmsForMasking 3R & done
 
 wait 
 
 mkdir rawFVFiles && mv exampleApplication/*.fvec rawFVFiles/
 mkdir trainingSets
-diploSHIC makeTrainingSets rawFVFiles/neut.msOut.gz.diploid.fvec rawFVFiles/soft \
+python3 ./packages/bin/diploSHIC makeTrainingSets rawFVFiles/neut.msOut.gz.diploid.fvec rawFVFiles/soft \
 rawFVFiles/hard 5 0,1,2,3,4,6,7,8,9,10 trainingSets/
 
-diploSHIC train trainingSets/ trainingSets/ bfsModel
+python3 ./packages/bin/diploSHIC train trainingSets/ trainingSets/ bfsModel
 
-diploSHIC fvecVcf diploid \
+python3 ./packages/bin/diploSHIC fvecVcf diploid \
 exampleApplication/ag1000g.phase1.ar3.pass.biallelic.3R.vcf.28000000-29000000.gz 3R 53200684 \
-exampleApplication/ag1000g.phase1.ar3.pass.biallelic.3R.vcf.28000000-29000000.gz.diploid.fvec \
+rawFVFiles/ag1000g.phase1.ar3.pass.biallelic.3R.vcf.28000000-29000000.gz.diploid.fvec \
 --targetPop BFS --sampleToPopFileName exampleApplication/samples_pops.txt --winSize 55000 \ 
 --maskFileName exampleApplication/Anopheles-gambiae-PEST_CHROMOSOMES_AgamP3.accessible.fa.gz
-diploSHIC predict bfsModel.json bfsModel.weights.hdf5 rawFVFiles/ag1000g.phase1.ar3.pass.biallelic.3R.vcf.28000000-29000000.gz.diploid.fvec mossie.preds
+python3 ./packages/bin/diploSHIC predict bfsModel.json bfsModel.weights.hdf5 rawFVFiles/ag1000g.phase1.ar3.pass.biallelic.3R.vcf.28000000-29000000.gz.diploid.fvec mossie.preds
 cp -r rawFVFiles /staging/vkhanna7
 cp -r trainingSets /staging/vkhanna7 
