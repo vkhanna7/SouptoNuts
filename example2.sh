103505906928daba755d7eb78368ae7d517a80a4
vkhanna7 <vkhanna7@submit2.chtc.wisc.edu>
2022-10-11 11:25:48 -0500

    example2.sh final

--- example2.sh
+++ example2.sh
@@ -3,24 +3,25 @@ cp /staging/vkhanna7/diploshic.tar.gz ./
 cp -r /staging/vkhanna7/exampleApplication ./
 cp -r /staging/vkhanna7/rawFVFiles ./
 cp -r /staging/vkhanna7/trainingSets ./
-ENVNAME=diploshic
-ENVDIR=$ENVNAME
-mkdir $ENVDIR
-export PATH
-tar -xzf $ENVNAME.tar.gz -C $ENVDIR
-. $ENVDIR/bin/activate
+
+tar -xzf packages.tar.gz 
+tar -xzf python39.tar.gz
+export PATH=$PWD/python/bin:$PATH
+export PYTHONPATH=$PWD/packages
+export HOME=$PWD
+export PATH=$PWD/packages/bin/:$PATH
+#rm diploshic.tar.gz
 
 
-rm diploshic.tar.gz
 
-diploSHIC train trainingSets/ trainingSets/ bfsModel
+python3 ./packages/bin/diploSHIC train trainingSets/ trainingSets/ bfsModel
 
-diploSHIC fvecVcf diploid \
+python3 ./packages/bin/diploSHIC fvecVcf diploid \
 exampleApplication/ag1000g.phase1.ar3.pass.biallelic.3R.vcf.28000000-29000000.gz 3R 53200684 \
-exampleApplication/ag1000g.phase1.ar3.pass.biallelic.3R.vcf.28000000-29000000.gz.diploid.fvec \
+rawFVFiles/ag1000g.phase1.ar3.pass.biallelic.3R.vcf.28000000-29000000.gz.diploid.fvec \
 --targetPop BFS --sampleToPopFileName exampleApplication/samples_pops.txt --winSize 55000 \
 --maskFileName exampleApplication/Anopheles-gambiae-PEST_CHROMOSOMES_AgamP3.accessible.fa.gz
-diploSHIC predict bfsModel.json bfsModel.weights.hdf5 rawFVFiles/ag1000g.phase1.ar3.pass.biallelic.3R.vcf.28000000-29000000.gz.diploid.fvec mossie.preds
+python3 ./packages/bin/diploSHIC predict bfsModel.json bfsModel.weights.hdf5 rawFVFiles/ag1000g.phase1.ar3.pass.biallelic.3R.vcf.28000000-29000000.gz.diploid.fvec mossie.preds
 cp -r rawFVFiles /staging/vkhanna7
 cp -r trainingSets /staging/vkhanna7
 
